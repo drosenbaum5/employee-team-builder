943ebc54d25272e9253b34ba0c699dccce2d9bd7
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-04-17 09:15:08 -0400

    Added classes for Intern and Engineer

---
+++ index.js
@@ -0,0 +1,48 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+// Starts the app
+menu = () => {
+ createManager = () => {
+     inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the managers name",
+            // Validation here
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "What is the team manager's id?",
+            // Validation here
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "What is the team manager's email?",
+            // Validation here
+          },
+          {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the team manager's office number?",
+            // Validation here
+          },
+     ]).then(({name, id, email, officeNumber}) => {
+         const manager = new Manager(name, id, email, officeNumber);
+         console.log(manager);
+       
+     })
+ }
+
+ createManager();
+ 
+}
+
+
+menu();
