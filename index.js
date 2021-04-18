f7dc2a94bf5075107127ec259fcf701d6ade7814
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-04-18 10:17:52 -0400

    separated questions into questions.js file and exported them as a module. Began to flesh out build team function

--- index.js
+++ index.js
@@ -2,47 +2,40 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const { managerQuestions, teamQuestions, engineerQuestions, interQuestions } = require("./questions");
 const path = require("path");
 const fs = require("fs");
 
 // Starts the app
 menu = () => {
- createManager = () => {
-     inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter the managers name",
-            // Validation here
-          },
-          {
-            type: "input",
-            name: "id",
-            message: "What is the team manager's id?",
-            // Validation here
-          },
-          {
-            type: "input",
-            name: "email",
-            message: "What is the team manager's email?",
-            // Validation here
-          },
-          {
-            type: "input",
-            name: "officeNumber",
-            message: "What is the team manager's office number?",
-            // Validation here
-          },
-     ]).then(({name, id, email, officeNumber}) => {
-         const manager = new Manager(name, id, email, officeNumber);
-         console.log(manager);
-       
-     })
- }
+  createManager = () => {
+    inquirer.prompt(managerQuestions).then(({ name, id, email, officeNumber }) => {
+      const manager = new Manager(name, id, email, officeNumber);
+      console.log(manager);
+      buildTeam()
+    });
+    buildTeam = () => {
+      inquirer
+        .prompt(teamQuestions)
+        .then((choice) => {
+          console.log(choice);
+          switch (choice) {
+            case "Engineer":
+              // createEngineer();
+              break;
+            case "Intern":
+              // createIntern();
+              break;
 
- createManager();
- 
-}
+            default:
+              // buldTeam();
+              break;
+          }
+        });
+    };
+  };
 
+  createManager();
+};
 
 menu();
